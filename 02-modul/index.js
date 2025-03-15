7c61754b450a22a81efe8b6254655970861560b7
agungstd <apakatadunia11@gmail.com>
2025-03-15 14:31:13 +0700

    Update Node.js core modules to modern ESM syntax

--- 02-modul/index.js
+++ 02-modul/index.js
@@ -1,41 +1,111 @@
-// import fs from "fs";
-// fs.readFile("file.txt", (err, data) => {
-//   if (err) throw err;
-//   console.log(data.toString());
-// });
+// File System (fs) with promises API
+import { readFile } from 'fs/promises';
 
-// import url from "url";
+async function readFileExample() {
+  try {
+    const data = await readFile('file.txt', 'utf8');
+    console.log('File content:', data);
+  } catch (err) {
+    console.error('Error reading file:', err);
+  }
+}
 
-// let adr = "http://localhost:8080/default.htm?year=2017&month=february";
-// let q = url.parse(adr, true);
-// console.info(q.host);
-// console.info(q.pathname);
-// console.log(q.query);
-// console.log(q.search);
+// URL module (modern approach)
+import { URL } from 'url';
 
-// import os from "os";
+function urlExample() {
+  const adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+  const myURL = new URL(adr);
+  
+  console.info('Host:', myURL.host);
+  console.info('Pathname:', myURL.pathname);
+  console.info('Query params:', Object.fromEntries(myURL.searchParams));
+  console.info('Search string:', myURL.search);
+}
 
-// console.info(os.platform());
-// console.info(os.homedir());
-// console.info(os.hostname());
-// console.table(os.cpus());
+// OS module
+import os from 'os';
 
-// import path from "path";
+function osExample() {
+  console.info('Platform:', os.platform());
+  console.info('Home directory:', os.homedir());
+  console.info('Hostname:', os.hostname());
+  console.table(os.cpus());
+  
+  // Additional useful OS information
+  console.info('Total memory (GB):', (os.totalmem() / 1024 / 1024 / 1024).toFixed(2));
+  console.info('Free memory (GB):', (os.freemem() / 1024 / 1024 / 1024).toFixed(2));
+  console.info('Uptime (hours):', (os.uptime() / 60 / 60).toFixed(2));
+}
 
-// let filePath = "c:/nodejs/02-modul/file.txt";
-// console.info(path.basename(filePath));
-// console.info(path.dirname(filePath));
-// console.info(path.extname(filePath));
-// console.info(path.parse(filePath).base);
+// Path module
+import path from 'path';
 
-import querystring from "querystring";
+function pathExample() {
+  const filePath = 'c:/nodejs/02-modul/file.txt';
+  console.info('Basename:', path.basename(filePath));
+  console.info('Directory name:', path.dirname(filePath));
+  console.info('File extension:', path.extname(filePath));
+  console.info('Parsed base:', path.parse(filePath).base);
+  
+  // Additional path operations
+  console.info('Normalized path:', path.normalize('c:/nodejs//02-modul/../file.txt'));
+  console.info('Joined path:', path.join('/nodejs', 'modules', 'example', 'file.txt'));
+  console.info('Absolute path:', path.resolve('file.txt'));
+}
 
-let obj = {
-  name: "Pojok Code",
-  age: 20,
-};
+// QueryString module (both modern and legacy approaches)
+import querystring from 'querystring';
+import { URLSearchParams } from 'url';
 
-obj.year = 2017;
+function querystringExample() {
+  // Legacy querystring approach
+  let obj = {
+    name: "Pojok Code",
+    age: 20,
+    year: 2017
+  };
+  
+  // Add additional properties
+  obj.location = "Jakarta";
+  obj.isActive = true;
+  
+  const qrLegacy = querystring.stringify(obj);
+  console.log('Legacy querystring result:', qrLegacy);
+  
+  // Modern URLSearchParams approach (recommended)
+  const params = new URLSearchParams();
+  Object.entries(obj).forEach(([key, value]) => {
+    params.append(key, value);
+  });
+  
+  console.log('Modern URLSearchParams result:', params.toString());
+  
+  // Parsing query strings
+  const parsedLegacy = querystring.parse(qrLegacy);
+  console.log('Parsed legacy querystring:', parsedLegacy);
+  
+  // Parse using URLSearchParams
+  const parsedModern = Object.fromEntries(new URLSearchParams(params.toString()));
+  console.log('Parsed modern querystring:', parsedModern);
+}
 
-const qr = querystring.stringify(obj);
-console.log(qr);
+// Execute all examples
+async function main() {
+  console.log('--- File System Example ---');
+  await readFileExample();
+  
+  console.log('\n--- URL Example ---');
+  urlExample();
+  
+  console.log('\n--- OS Example ---');
+  osExample();
+  
+  console.log('\n--- Path Example ---');
+  pathExample();
+  
+  console.log('\n--- QueryString Example ---');
+  querystringExample();
+}
+
+main().catch(console.error);
